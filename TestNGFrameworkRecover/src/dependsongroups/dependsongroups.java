4b1533feb4a100d11fc8ae4376472d4da6c63a07
Rajkumar10Siva <rkchella17@gmail.com>
2024-06-26 16:28:18 +0530

    Full depends on groups code added and updated

--- TestNGFrameworkRecover/src/dependsongroups/dependsongroups.java
+++ TestNGFrameworkRecover/src/dependsongroups/dependsongroups.java
@@ -1,5 +1,34 @@
 package dependsongroups;
 
+import org.testng.annotations.Test;
+
 public class dependsongroups {
+	
+	
+
+	@Test(groups = "SmokeTesting")
+	public static void smoke1()
+	{
+		System.out.println("Positive Login functionality code here");
+	}
+	
+	@Test(groups = "SmokeTesting")
+	public static void smoke2()
+	{
+		System.out.println("Positive Registration functionality code here");
+	}
+	
+	@Test(groups = "SmokeTesting")
+	public static void smoke3()
+	{
+		System.out.println("Product page here");
+	}
+	
+	
+	@Test(groups="sanity", dependsOnGroups = "SmokeTesting")
+	public static void sanity()
+	{
+		System.out.println("password visibility functionality added here");
+	}
 
 }
